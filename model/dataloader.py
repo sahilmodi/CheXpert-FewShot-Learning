a06a784f573e7cec71fd02e6211f481959f508c8
Sahil Modi <sm34524@gmail.com>
2021-04-02 16:31:54 -0500

    finished dataloader

--- model/dataloader.py
+++ model/dataloader.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pathlib import Path
 from yacs.config import CfgNode
 
+import torch
 import PIL.Image as Image
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
@@ -10,24 +11,23 @@ class ChexpertDataset(Dataset):
     def __init__(self, csv_path: Path, data_augmentation=False) -> None:
         super(ChexpertDataset, self).__init__()
         self.data_path = Path(csv_path).parent
-        self.annotations = pd.read_csv(csv_path)
+        self.annotations = pd.read_csv(csv_path).fillna(0)
         self.transforms = None
-        self.height, self.width = 224
+        self.height, self.width = 224, 224
         self.transforms = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Resize((self.height, self.width))
+                transforms.Resize((self.height, self.width)),
+                transforms.ToTensor()
             ])
         if data_augmentation:
             self.transforms = transforms.Compose([
-                transforms.ToTensor(),
                 transforms.RandomAffine(
                     degrees=(-15, 15),
                     translate=(0.05, 0.05),
                     scale=(0.95, 1.05)
                 ),
-                transforms.Resize((self.height, self.width))
+                transforms.Resize((self.height, self.width)),
+                transforms.ToTensor()
             ])
-            
 
     def __len__(self) -> int:
         return self.annotations.shape[0]
@@ -35,11 +35,13 @@ class ChexpertDataset(Dataset):
     def __getitem__(self, index: int) -> None:
         annotation = self.annotations.iloc[index]
         image = Image.open(self.data_path.parent / annotation['Path'])
+        classes = annotation[['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Pleural Effusion']].values.astype("uint8")
         data = self.transforms(image)
+        return data, torch.from_numpy(classes)
 
 
 def build_dataloader(cfg: CfgNode, split, data_augmentation=False):
-    valid_splits = ["train, valid, test"]
+    valid_splits = ["train", "valid", "test"]
     assert split in valid_splits, f"{split} should be one of {valid_splits}."
 
     ds_path = Path(cfg.PATH)
@@ -48,6 +50,3 @@ def build_dataloader(cfg: CfgNode, split, data_augmentation=False):
 
     dataset = ChexpertDataset(ds_path / f"{split}.csv", data_augmentation)
     return DataLoader(dataset, batch_size=bs, num_workers=num_workers, shuffle=split == "train")
-    
-
-val_dl = build_dataloader("../CheXpert-v1.0-small", data_augmentation=True, config={"batch_size": 100, "num_workers":8, })
