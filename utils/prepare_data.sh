fdec460db69d193a89100b5edcd0bb58a102b774
Sahil Modi <sm34524@gmail.com>
2021-04-07 00:33:12 -0500

    fixed typo

--- utils/prepare_data.sh
+++ utils/prepare_data.sh
@@ -1,6 +1,6 @@
 data_path=$1/CheXpert-v1.0-small
 zip_path=$data_path.zip
-[ ! -d $zip_path ] && curl http://download.cs.stanford.edu/deep/CheXpert-v1.0-small.zip --output $zip_path
+[ ! -f $zip_path ] && curl http://download.cs.stanford.edu/deep/CheXpert-v1.0-small.zip --output $zip_path
 unzip -q $zip_path -d $1
 rm $zip_path
 echo Saved to $data_path
