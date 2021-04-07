02e63bf3fcf6f561c76524c47de63711e21cd8f2
Sahil Modi <sm34524@gmail.com>
2021-04-07 13:33:37 -0500

    normalized val and test datasets and added train size config

--- chexpert_specific/model/dataloader.py
+++ chexpert_specific/model/dataloader.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 from pathlib import Path
 
@@ -10,7 +11,7 @@ from torch.utils.data import Dataset, DataLoader
 from utils.config import _C as cfg
 
 class ChexpertDataset(Dataset):
-    def __init__(self, csv_path: Path, data_augmentation=False) -> None:
+    def __init__(self, csv_path: Path, split) -> None:
         super(ChexpertDataset, self).__init__()
         self.data_path = Path(csv_path).parent
         self.annotations = pd.read_csv(csv_path).fillna(0)
@@ -18,21 +19,22 @@ class ChexpertDataset(Dataset):
         self.height, self.width = 224, 224
         self.transforms = transforms.Compose([
                 transforms.Resize((self.height, self.width)),
-                transforms.ToTensor()
-            ])
-        if data_augmentation:
-            self.transforms = transforms.Compose([
-                transforms.RandomAffine(
-                    degrees=(-15, 15),
-                    translate=(0.05, 0.05),
-                    scale=(0.95, 1.05)
-                ),
-                transforms.Resize((self.height, self.width)),
                 transforms.ToTensor(),
                 transforms.Normalize(128, 64),
                 transforms.ToPILImage(),
                 transforms.Lambda(lambda x: transforms.functional.equalize(x)),
                 transforms.ToTensor(),
+            ])
+        if split == "train":
+            assert cfg.DATA.BATCH_SIZE <= cfg.DATA.TRAIN_SIZE, "Batch size must be smaller than train size."
+            self.annotations = self.annotations.sample(frac=1).reset_index(drop=True)[:cfg.DATA.TRAIN_SIZE]
+            self.transforms = transforms.Compose([
+                self.transforms,
+                transforms.RandomAffine(
+                    degrees=(-15, 15),
+                    translate=(0.05, 0.05),
+                    scale=(0.95, 1.05)
+                ),
             ])
 
     def __len__(self) -> int:
@@ -53,5 +55,5 @@ def build_dataloader(split):
     ds_path = Path(cfg.DATA.PATH)
     bs = cfg.DATA.BATCH_SIZE
 
-    dataset = ChexpertDataset(ds_path / f"{split}.csv", data_augmentation=split=="train")
-    return DataLoader(dataset, batch_size=bs, num_workers=min(os.cpu_count(), 12), shuffle=split == "train")
+    dataset = ChexpertDataset(ds_path / f"{split}.csv", split)
+    return DataLoader(dataset, batch_size=bs, num_workers=min(os.cpu_count(), 0), shuffle=split == "train")
