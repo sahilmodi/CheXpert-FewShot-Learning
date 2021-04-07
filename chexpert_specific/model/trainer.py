a1a125702347f522c9e1645c68f62fcd211c903b
Sahil Modi <sm34524@gmail.com>
2021-04-06 22:10:16 -0500

    added tb writing

--- chexpert_specific/model/trainer.py
+++ chexpert_specific/model/trainer.py
@@ -30,24 +30,24 @@ class Trainer():
         self.writer = SummaryWriter(self.output_dir, flush_secs=60)
 
     def train(self):
-        t = tqdm(range(1, self.max_iters + 1))
+        t = tqdm(range(self.max_iters))
         t.update(self.iterations)
         for epoch in t:
-            self.train_epoch()
+            self.train_epoch(t)
             self.scheduler.step()
+        t.close()
     
-    def train_epoch(self):
+    def train_epoch(self, t):
         self.model.train()
 
-        print_every, val_every = 100, 300
-        train_loss, train_acc = 0, 0    
+        train_loss, train_acc = 0, 0
         cross_entropy_loss = nn.CrossEntropyLoss()
-        
+
         labels_, ys, train_loss, train_acc = [], [], [], []
         if self.mixup_alpha:
             ys_mixup, train_loss_mixup, train_acc_mixup = [], [], []
             
-        for batch_idx, (imgs, labels) in enumerate(tqdm(self.train_loader, position=1, leave=False)):
+        for batch_idx, (imgs, labels) in enumerate(self.train_loader):
             imgs, labels = imgs.to(self.device), labels.to(self.device)
             self.optimizer.zero_grad(set_to_none=True)
 
@@ -94,7 +94,8 @@ class Trainer():
             self.optimizer.step()
             self.iterations += 1
                 
-            if batch_idx % print_every == 0 and batch_idx != 0:
+            if batch_idx % 10 == 0 and batch_idx != 0:
+                self.writer.add_scalar("train/loss", train_loss[-1], self.iterations)
                 # log(train_writer, optimizer, iteration, train_loss, train_acc)
                 # log_aps(train_writer, iteration, acts, ys)
                             
@@ -103,6 +104,10 @@ class Trainer():
                 if self.mixup_alpha:
                     train_loss_mixup, train_acc_mixup = [], []
                     ys_mixup = []
+
+            t.update()
+            if self.iterations >= self.max_iters:
+                break
         
         # validation
         if self.mixup_alpha: 
@@ -128,52 +133,50 @@ class Trainer():
         if self.mixup:
             ys_mixup, val_loss, mixup = [], [], []
         
-        with torch.no_grad():
-            for batch_idx, (imgs, labels) in enumerate(self.val_loader):
-                imgs, labels = imgs.to(self.device), labels.to(self.device) 
+        for batch_idx, (imgs, labels) in enumerate(tqdm(self.val_loader, position=1, leave=False)):
+            imgs, labels = imgs.to(self.device), labels.to(self.device) 
 
-                # forward pass
+            # forward pass
+            with torch.no_grad():
                 y = self.model(imgs)
                 loss = cross_entropy_loss(y, labels)
                 
-                # accuracy
-                pred = y.argmax(dim=1, keepdim=True)        # get the index of the max log-probability
+            # accuracy
+            pred = y.argmax(dim=1, keepdim=True)        # get the index of the max log-probability
 
-                labels_.append(labels.detach().cpu().numpy())
-                ys.append((F.softmax(y, dim=1)).detach().cpu().numpy())
-                val_loss += [loss.item()]
-                val_acc += [pred.eq(a_tm1.view_as(pred)).float().mean().item()]
+            labels_.append(labels.detach().cpu().numpy())
+            ys.append((F.softmax(y, dim=1)).detach().cpu().numpy())
+            val_loss += [loss.item()]
+            val_acc += [pred.eq(a_tm1.view_as(pred)).float().mean().item()]
 
-                if self.mixup_alpha:
-                    # what is alpha --> see mixup reference
-                    # "additional samples" --> is there a regular non-mixup loss?
-                    # y_bar equation in paper is not used?
+            if self.mixup_alpha:
+                # what is alpha --> see mixup reference
+                # "additional samples" --> is there a regular non-mixup loss?
+                # y_bar equation in paper is not used?
 
-                    # generate mixup parameter
-                    lambda_ = np.random.beta(self.mixup_alpha, self.mixup_alpha)
+                # generate mixup parameter
+                lambda_ = np.random.beta(self.mixup_alpha, self.mixup_alpha)
 
-                    inds1 = torch.arange(self.batch_size)
-                    inds2 = torch.randperm(self.batch_size)
+                inds1 = torch.arange(self.batch_size)
+                inds2 = torch.randperm(self.batch_size)
 
-                    x_bar = lambda_ * imgs[inds1] + (1. - lambda_) * imgs[inds2]
+                x_bar = lambda_ * imgs[inds1] + (1. - lambda_) * imgs[inds2]
 
-                    # forward pass
-                    y_bar = self.model(x_bar)
-                    
-                    bce_loss = nn.BCELoss()
-                    loss_mixup = lambda_ * bce_loss(y_bar, labels[inds1]) + (1. - lambda_) * bce_loss(y_bar, labels[inds2])
-                    loss_mixup = loss_mixup.sum()
+                # forward pass
+                y_bar = self.model(x_bar)
+                
+                bce_loss = nn.BCELoss()
+                loss_mixup = lambda_ * bce_loss(y_bar, labels[inds1]) + (1. - lambda_) * bce_loss(y_bar, labels[inds2])
+                loss_mixup = loss_mixup.sum()
 
-                    loss += loss_mixup
+                loss += loss_mixup
 
-                    val_loss_mixup += [loss_mixup.item()]
-                    pred_mixup = y_bar.argmax(dim=1, keepdim=True)
-                    ys_mixup.append((F.softmax(y_bar, dim=1)).detach().cpu().numpy())
-                    val_acc_mixup += [pred_mixup.eq(labels.view_as(pred_mixup)).float().mean().item()]
+                val_loss_mixup += [loss_mixup.item()]
+                pred_mixup = y_bar.argmax(dim=1, keepdim=True)
+                ys_mixup.append((F.softmax(y_bar, dim=1)).detach().cpu().numpy())
+                val_acc_mixup += [pred_mixup.eq(labels.view_as(pred_mixup)).float().mean().item()]
 
-                j += 1
-                if j == print_every:
-                    break
+        self.writer.add_scalar("val/loss", np.mean(val_loss), self.iterations)
                 
         if self.mixup_alpha:
             return val_loss, val_acc, labels_, ys, val_loss_mixup, val_acc_mixup, ys_mixup
